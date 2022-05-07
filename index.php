f0fae9dc5c39db09cc9eea649a80c5996a61f001
Bukunmi Olarinde <Olarindebukunmi@gmail.com>
2022-05-07 08:05:24 -0700

    FILE STRUCTURE CREATED
    Created all the needed files for the script
    together with the PHPMailer package

--- index.php
+++ index.php
@@ -4,3 +4,44 @@
 
 ?>
 
+<!DOCTYPE html>
+         <html lang="en">
+         <head>
+         <title>send email to recepient</title>
+         <?php include 'metatags.php' ; ?>
+
+       <link rel="stylesheet" type="text/css" href="index.css">     
+  </head>
+
+       <body>
+
+                <div class="sub-container" >
+                	<p>Type your message below and send it instantly</p>
+                	<div class="visitor-details">
+                	<form action="#">
+                				<div class="error"></div><br>
+                	<div class="name-details">
+                	<label>Your Full Name:</label>
+                	<input type="text" name="fullname" placeholder="" pattern="[A-Z\sa-z]{3,25}" autofocus required>
+                	</div>
+                		 
+                	<div class="email-details">
+                	<label>Your E-mail Address:</label>
+                	<input type="email" id="pass" name="email" placeholder="" required autocomplete="off">
+                	</div>
+                		 
+                	<div class="textarea-details">
+                	<label>Your Message:</label>
+                	<textarea name="messagebody"></textarea>
+                	</div>
+                		 
+                	<input type="submit" class="submit" name="" value="Send Message">
+                		 
+                	</form>  
+                		 
+                	</div>
+                		 
+                		 
+                </div>
+	   </body>
+		 </html>
